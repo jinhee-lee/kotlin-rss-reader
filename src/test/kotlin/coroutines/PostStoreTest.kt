cb18d1628fac2afe3a20d32cbb6b95470fe6a94c
kingkong <jinhee.lee7@nhnent.com>
2025-04-22 14:07:28 +0900

    2일차 @Test 코드 작성 - v2

--- src/test/kotlin/coroutines/PostStoreTest.kt
+++ src/test/kotlin/coroutines/PostStoreTest.kt
@@ -3,13 +3,14 @@ package coroutines
 import corountines.client.Channel
 import corountines.domain.PostStore
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 
 class PostStoreTest {
     @Test
-    fun test_persist_and_read() =
+    fun test_persist_all() =
         runTest {
             val postStore =
                 PostStore(
@@ -19,8 +20,20 @@ class PostStoreTest {
                     ),
                 )
 
-            val job = launch { postStore.persistAll() }
-            job.join()
-            postStore.posts.size shouldBe 5
+            repeat(3) {
+                val job =
+                    launch(Dispatchers.IO) {
+                        println("Job1 > in ${Thread.currentThread()}")
+                        postStore.persistAll()
+                    }
+                job.join()
+
+                val checkJob =
+                    launch {
+                        println("Job2 > in ${Thread.currentThread()}")
+                        postStore.posts.size shouldBe (it + 1) * 5
+                    }
+                checkJob.join()
+            }
         }
 }
