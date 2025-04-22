cc844ba30005fd9cfef430876c702f3bca3201ae
kingkong <jinhee.lee7@nhnent.com>
2025-04-22 10:56:19 +0900

    2일차 posts2, posts3 추가

--- src/main/kotlin/corountines/Application.kt
+++ src/main/kotlin/corountines/Application.kt
@@ -5,10 +5,13 @@ import corountines.domain.Post
 import corountines.domain.PostStore
 import corountines.view.InputView
 import corountines.view.OutputView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 
 fun main() =
     runBlocking {
@@ -37,4 +40,17 @@ private suspend fun List<Channel>.posts(): List<Post> {
             async { it.findPosts() }
         }.awaitAll().flatten()
     }
+}
+
+private suspend fun List<Channel>.posts2(): List<Post> {
+    return withContext(Dispatchers.IO) {
+        map {
+            async { it.findPosts() }
+        }.awaitAll().flatten()
+    }
+}
+
+private suspend fun CoroutineScope.posts3(channels: List<Channel>): List<Post> {
+    return channels.map { async { it.findPosts() } }
+        .awaitAll().flatten()
 }
